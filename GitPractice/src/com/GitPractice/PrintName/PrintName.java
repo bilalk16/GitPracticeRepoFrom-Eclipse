5c4404c022717461fa3511c9837c785b1d5f619a
Bilalk16 <bilal.khd07@gmail.com>
2022-04-05 17:22:00 -0400

    tried to push GitPractice project to github web

---
+++ GitPractice/src/com/GitPractice/PrintName/PrintName.java
@@ -0,0 +1,17 @@
+package com.GitPractice.PrintName;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Bilal");
+		
+		System.out.println("my Another println"); // any new change but no commit will come with error mark which says commmit is pending. so i need to commit it.. new changees will be unstaged.. not need to update again
+
+		
+	// My changes today
+		
+		System.out.println("NEw changes for 4-5-22");
+		
+	}
+	
+}
